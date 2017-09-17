411985e05d9ee57eaf55b2ee56c6c55604e7ee1b
iceberg12 <tuanminh.nus@gmail.com>
2017-09-17 20:08:06 +0800

    1st commit

---
+++ src/MPC.h
@@ -0,0 +1,27 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+using namespace std;
+
+const double latency = 0.1;  //latency in seconds
+const double dt = 0.05;
+const int latency_ind = 2; //latency in units of dt
+
+class MPC {
+ public:
+  MPC();
+
+  virtual ~MPC();
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  
+  double delta_prev {0};
+  double a_prev {0.1};
+};
+
+#endif /* MPC_H */
